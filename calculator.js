8bf417b692e6e3e779da3a345ac4b1da2f4675cf
HIMANSHU DUBEY <113777911+Himanshu1128@users.noreply.github.com>
2023-07-12 11:31:47 +0530

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -6,7 +6,7 @@ let output = "";
 const calculate = (btnValue) => {
   display.focus();
   if (btnValue === "=" && output !== "") {
-    output = eval(output.replace("%", "/100"));
+    output = eval(inputE.value);
   } else if (btnValue === "AC") {
     output = "";
   } else if (btnValue === "DEL") {
